8b5ea3783ab1bcb9800d1e531b7000a3437a215e
jucordero <obejaamarilla@hotmail.com>
2024-09-14 16:42:36 +0100

    match date, time, injuries, etc

--- app.py
+++ app.py
@@ -7,39 +7,51 @@ from player_list import players
 # Create a boolean array of the same length as the number of players
 attendance = np.zeros(len(players), dtype=bool)
 pods = np.zeros(len(players), dtype=bool)
+injuries = np.zeros(len(players), dtype=bool)
 
 ncols =  6
+
+st.image("images/ezgif-7-96cb9745af.jpg", width=150)
+
+game_cols = st.columns(2)
+with game_cols[0]:
+    date = st.date_input("Match date")
+with game_cols[1]:
+    time = st.time_input("Kickoff time")
+
+opponent = st.text_input("Opponent", "Opponent team")
 
 cols_head = st.columns(ncols)
 with cols_head[0]:
     st.write("Player")
 with cols_head[1]:
-    st.write("Attended")
+    st.write("Attended match?")
 with cols_head[2]:
-    st.write("Brought pod to training")
+    st.write("Forgot to bring pod?")
 with cols_head[3]:
-    st.write("Goals scored")
+    st.write("Got injured?")
 with cols_head[4]:
+    st.write("Goals scored")
+with cols_head[5]:
     st.write("Assists")
     
 
 for i, plyr in enumerate(players):
     cols = st.columns(ncols)
     with cols[0]:
-        st.write("")
-        st.write("")
-        st.write("")
         st.write(plyr)
     with cols[1]:
-        st.write("")
-        st.write("")
-        attendance[i] = st.checkbox("Attended", label_visibility="hidden", key=f"attendance_{i}")
+        attendance[i] = st.checkbox("Attended", label_visibility="collapsed", key=f"attendance_{i}")
     with cols[2]:
-        st.write("")
-        st.write("")
-        pods[i] = st.checkbox("Pod", label_visibility="hidden", key=f"pod_{i}")
+        pods[i] = st.checkbox("Forgot pod?", label_visibility="collapsed", key=f"pod_{i}")
     with cols[3]:
-        st.text_input("Goals", key=f"goals_{i}", label_visibility="hidden")
+        injuries[i] = st.checkbox("Got injured?", label_visibility="collapsed", key=f"injury_{i}")
     with cols[4]:
-        st.text_input("Assits", key=f"assists_{i}", label_visibility="hidden")
+        st.text_input("Goals", key=f"goals_{i}", label_visibility="collapsed")
+    with cols[5]:
+        st.text_input("Assits", key=f"assists_{i}", label_visibility="collapsed")
+
+comments = st.text_area("Comments")
+
+st.button("Submit")
 
